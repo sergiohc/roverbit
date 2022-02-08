7a02d38a38d135e08fd055b3e87aabe68e254376
sergiohc <sergioh.c@live.com>
2022-02-08 15:57:27 -0300

    remove deprecation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   def index; end
 
